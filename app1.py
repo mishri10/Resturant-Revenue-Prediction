65435f35ebc5526790bf84acd22c5ba10fca63a7
Ridhima Mishra <100011986+mishri10@users.noreply.github.com>
2024-06-28 23:53:56 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -18,30 +18,30 @@ parking_encoding = {'Yes': 1, 'No': 0}
 
 # Sidebar for input
 st.sidebar.title('Input Options')
-location = st.sidebar.selectbox('Select Location', list(location_encoding.keys()))
-cuisine = st.sidebar.selectbox('Select Cuisine', list(cuisine_encoding.keys()))
-parking = st.sidebar.selectbox('Parking Availability', list(parking_encoding.keys()))
 
-rating = st.sidebar.slider('Rating', 0, 5, 1)
-seating_capacity = st.sidebar.slider('Seating Capacity', 30, 100, 100)
+cuisine = st.sidebar.selectbox('Select Cuisine', list(cuisine_encoding.keys()))
+
+
+rating = st.sidebar.slider('Rating', 0, 5, 4)
+seating_capacity = st.sidebar.slider('Seating Capacity', 30, 200, 100)
 average_meal_price = st.sidebar.slider('Average Meal Price', 200, 2000, 200)
 social_media_followers = st.sidebar.slider('Social Media Followers', 1000, 100000, 1000)
 chef_experience_years = st.sidebar.slider('Chef Experience Years', 0, 20, 2)
-number_of_reviews = st.sidebar.slider('Number of Reviews', 0, 2000, 2)
+
 weekend_reservations = st.sidebar.slider('Weekend Reservations', 0, 100, 1)
 weekday_reservations = st.sidebar.slider('Weekday Reservations', 0, 100, 1)
 
 # Prepare input data
 input_data = pd.DataFrame({
-    'Location': [location_encoding.get(location, 0)],
-    'Cuisine': [cuisine_encoding.get(cuisine, 0)],
+    'Location': [0],
+    'Cuisine': [cuisine_encoding.get(cuisine, 4)],
     'Rating': [rating],
     'Seating Capacity': [seating_capacity],
     'Average Meal Price': [average_meal_price],
     'Social Media Followers': [social_media_followers],
     'Chef Experience Years': [chef_experience_years],
-    'Number of Reviews': [number_of_reviews],
-    'Parking Availability': [parking_encoding.get(parking, 0)],
+    'Number of Reviews': [1000],
+    'Parking Availability': [0],
     'Weekend Reservations': [weekend_reservations],
     'Weekday Reservations': [weekday_reservations]
 })
@@ -49,7 +49,7 @@ input_data = pd.DataFrame({
 # Display the input options and the predicted result
 st.title('Restaurant Revenue Prediction')
 image=Image.open('img.jpeg')
-st.image(image, '')
+st.image(input_data.drop(columns=['Location', 'Number of Reviews', 'Parking Availability']))
 
 
 st.write('### Options Chosen')
